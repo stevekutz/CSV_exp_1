6272c0c3ec395d266847b16a0d92efbea1a4b868
stevekutz <stkutz@gmail.com>
2019-10-05 21:08:48 -0500

    rendering out CSV values with Loader Spinner

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Button, Container} from 'semantic-ui-react';
 import CSVReader from 'react-csv-reader';
-import 'react-loader-spinner';
+import Loader from 'react-loader-spinner';
 
 function App() {
  const [infoCSV, setInfoCSV] = useState([ ]);
@@ -24,16 +24,36 @@ function App() {
           label = 'Choose your CSV file'
           onFileLoaded = {handleData}
           onError = {handleError}
-          inputId="ObiWan"
+          inputId="CSVstyling"
           inputStyle={{border: '1px solid blue', color: 'deeppink'}}
           inputType = "color"
           inputValue="#e66465"
         />
       </Container>
 
-      <input type="color" id="head" name="head" readOnly value="#e66465"
-   />
+      <input type="color" id="head" name="head" readOnly value="#e66465"/>
 
+      <div style = {{ border: '1px solid red', display: 'flex', width: '100%', justifyContent: 'center'}}>
+      {infoCSV.length 
+        ? 
+        <Container>
+          <p>length is {infoCSV.length}</p>
+          {infoCSV.map(item => 
+            <ul key = {item[0]}>
+              <li>{item[0]}</li>
+              <li>{item[1]}</li>
+              <li>{item[2]}</li>
+              <li>{item[3]}</li>
+              <li>{item[4]}</li>
+            
+            </ul>
+            )}
+        </Container>
+        : 
+          <Loader width = {25} height= {25} type = 'Grid'/> 
+          
+      }
+      </div>
 
     </div>
   );
