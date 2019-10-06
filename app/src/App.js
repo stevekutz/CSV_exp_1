678b44a16f927c920c5cf7687a4b75e03ed3352e
stevekutz <stkutz@gmail.com>
2019-10-05 20:18:34 -0500

    exp with CSVReader styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Button, Container} from 'semantic-ui-react';
 import CSVReader from 'react-csv-reader';
-
+import 'react-loader-spinner';
 
 function App() {
  const [infoCSV, setInfoCSV] = useState([ ]);
@@ -11,17 +11,28 @@ function App() {
     setInfoCSV(data);
   }
 
+  const handleError = (err) => {
+    console.log('Error msg is >>>>> ', err);
+  }
+
   console.log('CSV info >>> ', infoCSV);
-  // console.log('CSV info >>> ', infoCSV);
+  
   return (
     <div >
-      <Container>
+      <Container  >
         <CSVReader
           label = 'Choose your CSV file'
           onFileLoaded = {handleData}
+          onError = {handleError}
+          inputId="ObiWan"
+          inputStyle={{border: '1px solid blue', color: 'deeppink'}}
+          inputType = "color"
+          inputValue="#e66465"
         />
-      
       </Container>
+
+      <input type="color" id="head" name="head" readOnly value="#e66465"
+   />
 
 
     </div>
