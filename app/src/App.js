ac2e8f92c76a43a904f658c4db5bf27193738c29
stevekutz <stkutz@gmail.com>
2019-10-06 21:20:10 -0500

    preparing to compare state methods

--- app/src/App.js
+++ app/src/App.js
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 import {Button, Container} from 'semantic-ui-react';
 import CSVReader from 'react-csv-reader';
 import Loader from 'react-loader-spinner';
-import hooks_CSVReader from './hooks_CSVReader';
 import Hooks_CSVReader from './hooks_CSVReader';
 
 function App() {
  
   
   return (
-    <div >
-      <Hooks_CSVReader />
+    <div style = {{border: '4px solid purple', display: 'flex', flexDirection: 'column'}}>
+      <div style = {{border: '1px solid seagreen', width: '50%'}}>
+        <Hooks_CSVReader />
+      </div>
+      
 
       
 
