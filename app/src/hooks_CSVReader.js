4ae7984d729ed6e580e3e300332bcece3a0963f7
stevekutz <stkutz@gmail.com>
2019-10-19 18:41:12 -0500

    dropdown label shows  current index chosen via value

--- app/src/hooks_CSVReader.js
+++ app/src/hooks_CSVReader.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Button, Container, Dropdown, Grid, Label, Select, GridColumn} from 'semantic-ui-react';
+import {Container, Dropdown, Grid, Label} from 'semantic-ui-react';
 import CSVReader from 'react-csv-reader';
 import Loader from 'react-loader-spinner';
 
@@ -16,7 +16,7 @@ function Hooks_CSVReader() {
   // const [fieldNames, setFieldNames] = useState(['label', 'country']);
   const [fieldNames, setFieldNames] = useState([]);
 
-  const [dropdownNames, setdropdownNames] = useState([]);
+  const [dropdownNames, setdropdownNames] = useState([{key: '', text: '', value: ''}]);
   const [dropdownSelected, addDropDownSelected] = useState(['default']);
   const [value, setValue] = useState(0);
 
@@ -25,12 +25,27 @@ function Hooks_CSVReader() {
   const FOUNDparent_id = "8b4eb7b4-893c-4bb2-8bbe-b75c4223854e";
 
   // Default fields needed
+  const [matchCSV, updatematchCSV] = useState({
+    index_student_first_name: '', // set this to CSV index that holds student first name
+    index_student_last_name: '',
+    index_parent_first_name: '', // set this to CSV index that holds student first name
+    index_parent_last_name: '',
+    index_parent_email: '',
+    index_parent_phone: '',
+  });
+
+  // 
   const [newDataRow, updateNewDataRow] = useState({
-    first_name: '', // set this to CSV index that holds student first name
-    last_name: '',
+    student_first_name: '', 
+    student_last_name: '',
+    parent_first_name: '', 
+    parent_last_name: '',
+    parent_email: '',
+    parent_phone: '',
     school_id: USERschool_id,
     parent_id: FOUNDparent_id,
   });
+
 
   const school_id = "4187269f-d1fa-41fe-ad34-2e7d74a9031a";
   const parent_id = "8b4eb7b4-893c-4bb2-8bbe-b75c4223854e";
@@ -121,125 +136,127 @@ function Hooks_CSVReader() {
       
   }, [infoCSV])
 
-  const options = [
-    { key: 1, text: 'One', value: 1 },
-    { key: 2, text: 'Two', value: 2 },
-    { key: 3, text: 'Three', value: 3 },
-  ]
-
-  const options2 = [
-    { key: 0, text: 'First', value: 'firsty' },
-    { key: 1, text: 'Second', value: 'secondy'},
-    { key: 2, text: 'Last', value: 'thirdy' },
-  ]
-
+  
   return (
     <div >
-
-      <Container  >
-        <CSVReader
-          label = 'Hooks-Choose your CSV file'
-          onFileLoaded = { (data) => handleData(data)}
-          onError = {handleError}
-          inputId="CSVstyling"
-          inputStyle={{border: '1px solid blue', color: 'deeppink'}}
-          inputType = "color"
-          inputValue="#e66465"
-        />
-      </Container>
-
-      <input type="color" id="head" name="head" readOnly value="#e66465"/>
-
-      <div style = {{ border: '1px solid red', display: 'flex', width: '100%', justifyContent: 'center'}}>
+    
+    <Container  >
+    <CSVReader
+    label = 'Hooks-Choose your CSV file'
+    onFileLoaded = { (data) => handleData(data)}
+    onError = {handleError}
+    inputId="CSVstyling"
+    inputStyle={{border: '1px solid blue', color: 'deeppink'}}
+    inputType = "color"
+    inputValue="#e66465"
+    />
+    </Container>
+    
+    <input type="color" id="head" name="head" readOnly value="#e66465"/>
+    
+    <div style = {{ border: '1px solid red', display: 'flex', width: '100%', justifyContent: 'center'}}>
+    
+    {loadingData 
+      ? 
+      <Loader style = {{border: '1px solid blue', display: 'flex', justifyContent: 'center'}}
+      width = {25} height= {25} color = 'purple' type = 'TailSpin'/> 
+      : null
+    }
+    
+    
+    
+    {fieldNames.length 
+      ? 
+      <Container>
+      <p>length is {fieldNames.length}</p>
       
-        {loadingData 
-            ? 
-                <Loader style = {{border: '1px solid blue', display: 'flex', justifyContent: 'center'}}
-                        width = {25} height= {25} color = 'purple' type = 'TailSpin'/> 
-            : null
-        }
       
-
-
-        {fieldNames.length 
-          ? 
-          <Container>
-            <p>length is {fieldNames.length}</p>
-            
-            
-            {fieldNames.map((item,id) => (
-                <li key = {id}>
-                  {item}  
-                </li>
-            )          
-              )}
-            
-              
-            <Grid columns = {2}>
-              <Grid.Column>
-                <Label> {value}</Label>
-                  <Dropdown
-                    text = "Student first name"
-                    options = {dropdownNames}
-                    fluid
-                    selection
-                    value = {value}
-                    onChange = {_handleDropDown}
-                />
-              </Grid.Column>
-              <Grid.Column>
-              <Label> {value}</Label>
-                <Dropdown
-                  text = "Student first name"
-                  options = {dropdownNames}
-                  fluid
-                  selection
-                  value = {value}
-                  onChange = {_handleDropDown}
-                />
-              </Grid.Column>
-            
-            
-            </Grid>  
-
-          </Container>
-          : 
-            null           
-        }
-
-
+      {fieldNames.map((item,id) => (
+        <li key = {id}>
+        {item}  
+        </li>
+        )          
+        )}
+        
+       
+        <Grid columns = {2} divided style = {{width: '50%'}}>
+          <Grid.Column>
+            <Label> Student First Name: {value}</Label>
+            <Dropdown
+            text = "Student first name"
+            options = {dropdownNames}
+            fluid
+            selection
+            value = {value}
+            onChange = {_handleDropDown}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Label> Student Last Name {value}</Label>
+            <Dropdown
+            text = "Student first name"
+            options = {dropdownNames}
+            fluid
+            selection
+            value = {value}
+            onChange = {_handleDropDown}
+            />
+          </Grid.Column>
+        
+        
+        </Grid>  
+        
+        </Container>
+        : 
+        null           
+      }
+      
+      
       </div>
+      
+      </div>
+      );
+    }
+    
+    export default Hooks_CSVReader;
+    
+    
+    /*
+    
+    // const newStudentPayload = {
+      //   ...studentInfo,
+      //   field_trip_id: match.params.id,
+      //   school_id: user.school_id
+      // };
+      
+      // const { first_name, last_name, field_trip_id, school_id, parent_id } = req.body;  
+      // const [newDataRow, updateNewDataRow] 
+      
+      
+      // // const [newData, addNewData] = useState({});
+      
+      // const _handle_CSV_Row = (fieldsByIndex, infoCSV) => {
+        //   for(let i = 1; i < infoCSV.length; i++) {
+          //     infoCSV[i]
+          
+          //   }
+          
+          
+          // }
+          
+          //showData(infoCSV);
+          
+          */
 
-    </div>
-  );
-}
 
-export default Hooks_CSVReader;
-
-
-/*
-  
-  // const newStudentPayload = {
-  //   ...studentInfo,
-  //   field_trip_id: match.params.id,
-  //   school_id: user.school_id
-  // };
-
-  // const { first_name, last_name, field_trip_id, school_id, parent_id } = req.body;  
-  // const [newDataRow, updateNewDataRow] 
-
-
-  // // const [newData, addNewData] = useState({});
-
-  // const _handle_CSV_Row = (fieldsByIndex, infoCSV) => {
-  //   for(let i = 1; i < infoCSV.length; i++) {
-  //     infoCSV[i]
-
-  //   }
-
-
-  // }
-
-  //showData(infoCSV);
-  
-*/
+            // const options = [
+            //   { key: 1, text: 'One', value: 1 },
+            //   { key: 2, text: 'Two', value: 2 },
+            //   { key: 3, text: 'Three', value: 3 },
+            // ]
+          
+            // const options2 = [
+            //   { key: 0, text: 'First', value: 'firsty' },
+            //   { key: 1, text: 'Second', value: 'secondy'},
+            //   { key: 2, text: 'Last', value: 'thirdy' },
+            // ]
