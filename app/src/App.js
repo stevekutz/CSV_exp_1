bbfc45e25440f6405bc0b17cbe7db18c08a1c153
stevekutz <stkutz@gmail.com>
2019-10-19 23:04:37 -0500

    added CSV header present slider

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,5 @@
-import React, {useState} from 'react';
-import {Button, Container} from 'semantic-ui-react';
-import CSVReader from 'react-csv-reader';
-import Loader from 'react-loader-spinner';
-import Hooks_CSVReader from './hooks_CSVReader';
+import React from 'react';
+import HooksCSVReader from './hooksCSVReader';
 // import State_CSVReader from './state_CSVReader';
 // import DropDownTest from './DropDownTest';
 
@@ -12,7 +9,7 @@ function App() {
   return (
     <div style = {{border: '4px solid purple', display: 'flex', flexDirection: 'column'}}>
       <div style = {{border: '1px solid seagreen', width: '80%', margin: '0 auto'}}>
-        <Hooks_CSVReader />
+        <HooksCSVReader />
         
       </div>
 
---
+++ app/src/hooksCSVReader.js
@@ -0,0 +1,244 @@
+import React, {useState, useEffect} from 'react';
+import {Button, Card, Checkbox, Container, Dropdown, Label} from 'semantic-ui-react';
+import CSVReader from 'react-csv-reader';
+import Loader from 'react-loader-spinner';
+
+
+ // const fieldsByIndex = {};
+ // const fieldNames = ['label', 'country'];   
+ //    const [fieldNames, setFieldNames] = useState(['StudentLastName',	'StudentFirstName',	
+ // 'Studentemail', 'ParentLastName',	'ParentFirstName', 'ParentPhone',	'Parentemail']);
+
+function HooksCSVReader() {
+  const [infoCSV, setInfoCSV] = useState([]);
+  const [headerToggle, setHeaderToggle] = useState({ checked: true, label: 'Header Present'});
+  const [loadingData, setLoading] = useState(false);
+  // const [fieldsByIndex, setFieldsByIndex] = useState({});
+  // const [fieldNames, setFieldNames] = useState(['label', 'country']);
+  const [fieldNames, setFieldNames] = useState([]);
+
+  const [dropdownNames, setdropdownNames] = useState([]);
+  //const [dropdownSelected, addDropDownSelected] = useState([{key: '', text: '', value: ''}]);
+  //const [value, setValue] = useState(0);
+
+  // fields that need to be gotten
+  const USERschool_id = "4187269f-d1fa-41fe-ad34-2e7d74a9031a";
+  const FOUNDparent_id = "8b4eb7b4-893c-4bb2-8bbe-b75c4223854e";
+
+  // Default fields needed
+  const [matchCSV, updateMatchCSV] = useState({
+    index_student_first_name: '', // set this to CSV index that holds student first name
+    index_student_last_name: '',
+    index_parent_first_name: '', 
+    index_parent_last_name: '',
+    index_parent_email: '',
+    index_parent_phone: '',
+  });
+
+  // 
+  
+  
+  
+  
+  const handleData = (data) => {
+    resetDropDown();
+    setInfoCSV([]);
+    setLoading(true);    
+    setTimeout( ()=> {
+      console.log('inside setTimeout');
+      console.log('inside setTimeout infoCSV >>> ', infoCSV);
+      setInfoCSV(data)  // MUST be here
+      setLoading(false); 
+    }, 2000);    
+    // setInfoCSV(data)   // NOT here
+    setFieldNames(data[0]); 
+  }
+  
+  const handleError = (err) => {
+    console.log('Error msg is >>>>> ', err);
+  }
+  
+  const createFieldTitle = (str) => {   
+    return str.split('_')
+    .splice(1, str.length-1).map( a => a.charAt(0).toUpperCase() + a.substr(1))
+    .join(' ')
+  };
+      
+    const _handleDropDown = (e, data) => {      
+        updateMatchCSV({
+          ...matchCSV,
+          [data.name]: data.value
+        })
+        
+        // set index to Number just to be safe
+        setdropdownNames([...dropdownNames], dropdownNames[Number(data.value)].disabled = true);
+        
+    }
+      
+    const scrubCSV = (fieldNames ) => {    
+        setdropdownNames(
+          fieldNames.map( (item, index) => {
+            return {key: index, text: item, value: index, disabled: false}
+          }))    
+    }
+    
+    const [newDataRow, updateNewDataRow] = useState({
+      student_first_name: '', 
+      student_last_name: '',
+      // parent_first_name: '', 
+      // parent_last_name: '',
+      // parent_email: '',
+      // parent_phone: '',
+      school_id: USERschool_id,
+      parent_id: FOUNDparent_id,
+    });
+  
+  
+    const school_id = "4187269f-d1fa-41fe-ad34-2e7d74a9031a";
+    const parent_id = "8b4eb7b4-893c-4bb2-8bbe-b75c4223854e";
+    
+    const createData = () => {
+
+      updateNewDataRow(
+        {
+          student_first_name: '', 
+          student_last_name: '',
+          // parent_first_name: '', 
+          // parent_last_name: '',
+          // parent_email: '',
+          // parent_phone: '',
+          school_id: USERschool_id,
+          parent_id: FOUNDparent_id,
+        }
+
+
+      )
+
+
+    }
+    
+    
+    useEffect(()=> {
+      console.log('!!!!!!!!!!!!  showing dropdownNames ', dropdownNames);
+      console.log('################  showing names ', matchCSV);
+      console.log('headerToggle',  headerToggle);
+    }, [dropdownNames, matchCSV, headerToggle] )
+    
+    
+    
+    useEffect(() => {  
+    console.log('InfoCSV is ', infoCSV);
+
+    // scrubCSV(fieldsByIndex, infoCSV, fieldNames);         
+    scrubCSV(fieldNames);       
+      
+  }, [infoCSV, fieldNames])
+
+  const resetDropDown = () => {
+    setdropdownNames([]);
+    setFieldNames([]);
+  }
+
+
+  const alignStyles = {width: '50%', margin: '5px auto', padding: '5px', display: 'flex'}                    
+  const cardStyles = {border: '1px solid deeppink', color: 'dodgerblue', ...alignStyles}
+  
+  const handleHeader = (e, data) => {
+    console.log('CHECKBOX data',  data);
+    setHeaderToggle(data.checked);
+
+  }
+
+  return (
+    <div >
+
+      <Container >
+      <CSVReader
+        label = 'Hooks-Choose your CSV file'
+        onFileLoaded = { (data) => handleData(data)}
+        onError = {handleError}
+        inputId="CSVstyling"
+        inputStyle={{border: '1px solid blue', color: 'deeppink'}}
+        inputType = "color"
+        inputValue="#e66465"
+      />
+      <div style = {{display: 'flex', justifyContent: 'space-evenly', padding: '10px'}}>
+        <Button  size = 'mini' color='red' onClick = {resetDropDown} > reset dropDowns </Button>
+        
+        <Checkbox slider = {true} onChange = {handleHeader} label = "Header Present" checked = {headerToggle.checked}/>
+        <Button  size = 'mini' color='green'  > Load CSV </Button>
+      
+      
+      </div>
+      </Container>
+    
+      
+      <div>
+      
+        {loadingData 
+          ? 
+          <Loader style = {{border: '1px solid blue', display: 'flex', justifyContent: 'center'}}
+          width = {25} height= {25} color = 'purple' type = 'TailSpin'/> 
+          : null
+        }
+        
+        {fieldNames.length 
+          ? 
+          <Container style = {{border: '1px solid green', display: 'flex', width: '60%', flexWrap: 'wrap'}}>
+                  
+            {Object.keys(matchCSV).map((item,id) => (
+              <Card key = {id} style = {cardStyles}>
+                <Label> {createFieldTitle(item)} </Label>
+                  <Dropdown
+                    options = {dropdownNames}
+                    search
+                    selection
+                    name = {item}
+                    onChange = {_handleDropDown}
+                  />  
+              </Card>
+            ))}  
+
+            </Container>
+            : 
+            null           
+          }
+        
+        
+        </div>
+      
+      </div>
+      );
+    }
+    
+    export default HooksCSVReader;
+
+
+
+
+      
+  // const findFieldIndex = (arr, fieldName, fieldNames) => {
+    //   if(arr.length) {
+      //     console.log('fieldnames in CSV are: ', fieldNames);
+      //     let currentIndex = null;
+      //     console.log('current fieldName', fieldName);
+      //  //   currentIndex = titleBar.indexOf(`${fieldName}`);
+      //     currentIndex = fieldNames.indexOf(`${fieldName}`);
+      //     console.log('curentIndex ', currentIndex);
+      //     return currentIndex;
+      //     }
+      //   return -1;
+      
+      // }
+      
+          // fieldNames.forEach( (element, index) => {
+            //     if(infoCSV.length) {
+              //      // console.log('current element is >> ', element);
+    //      fieldsByIndex[`${element}`] = findFieldIndex(infoCSV, element, fieldNames); // works
+    //      // setFieldsByIndex({...fieldsByIndex , [element]: findFieldIndex(infoCSV, element, fieldNames)  }) // nooo
+    //      // setFieldsByIndex( fieldsByIndex[`${element}`] = findFieldIndex(infoCSV, element)  ) // error on second run
+    //        console.log('Scrub FBI   ',fieldsByIndex);
+    
+    //     }
+    
+    // }); 
